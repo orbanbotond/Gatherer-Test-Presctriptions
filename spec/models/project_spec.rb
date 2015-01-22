3bfe6c04f08d303f7bf29da3e7068abcea2c1d52
Orban Botond <orbanbotond@gmail.com>
2015-01-22 16:39:17 +0200

    Spies mocks and doubles are added to the Project spec

--- spec/models/project_spec.rb
+++ spec/models/project_spec.rb
@@ -29,19 +29,15 @@ RSpec.describe Project do
       expect(project).to be_done
     end
 
-    #
     it "properly estimates a blank project" do
       expect(project.completed_velocity).to eq(0)
       expect(project.current_rate).to eq(0)
       expect(project.projected_days_remaining.nan?).to be_truthy
       expect(project).not_to be_on_schedule
     end
-    #
-
   end
 
   describe "estimates" do
-    #
     let(:project) { Project.new }
     let(:newly_done) { Task.new(size: 3, completed_at: 1.day.ago) }
     let(:old_done) { Task.new(size: 2, completed_at: 6.months.ago) }
@@ -51,18 +47,18 @@ RSpec.describe Project do
     before(:example) do
       project.tasks = [newly_done, old_done, small_not_done, large_not_done]
     end
-    #
 
-    it "calculates the total size" do
+    #
+    it "can calculate total size" do
       expect(project).to be_of_size(10)
       expect(project).to be_of_size(5).for_incomplete_tasks_only
     end
+    #
 
     it "can calculate remaining size" do
       expect(project.remaining_size).to eq(5)
     end
 
-    #
     it "knows its velocity" do
       expect(project.completed_velocity).to eq(3)
     end
@@ -81,8 +77,102 @@ RSpec.describe Project do
       project.due_date = 6.months.from_now
       expect(project).to be_on_schedule
     end
-
-    #
-
   end
+
+  #
+  it "stubs an object" do
+    project = Project.new(name: "Project Greenlight")
+    allow(project).to receive(:name) 
+    expect(project.name).to be_nil 
+  end
+  #
+
+  #
+  it "stubs an object again" do
+    project = Project.new(name: "Project Greenlight")
+    allow(project).to receive(:name).and_return("Fred") 
+    expect(project.name).to eq("Fred") 
+  end
+  #
+
+  #
+  it "stubs the class" do
+    allow(Project).to receive(:find).and_return(
+        Project.new(name: "Project Greenlight"))
+    project = Project.find(1) 
+    expect(project.name).to eq("Project Greenlight")
+  end
+#
+
+#
+  it "mocks an object" do
+    mock_project = Project.new(name: "Project Greenlight")
+    expect(mock_project).to receive(:name).and_return("Fred")
+    expect(mock_project.name).to eq("Fred")
+  end
+#
+
+#
+  xit "stubs with multiple returns" do
+    project = Project.new
+    allow(project).to receive(:user_count).and_return(1, 2)
+    assert_equal(1, project.user_count)
+    assert_equal(2, project.user_count)
+    assert_equal(2, project.user_count)
+  end
+#
+
+  context "for my understanding" do
+    it "generic doubles" do
+      x = double(:fn => "Boti", :m => "Masik")
+      expect(x.fn).to eq("Boti")
+      expect(x.m).to eq("Masik")
+      expect { x.asdsad }.to raise_error(Exception)
+    end
+    it "doubles as null objects aka spyes" do
+      x = double.as_null_object
+      expect{ x.asdsad }.to_not raise_error(Exception)
+      x = spy
+      expect{ x.asdsad }.to_not raise_error(Exception)
+    end
+    context "verifying" do
+      context "doubles" do
+        it "instances" do
+          x = instance_double( Project, :name => "Boti")
+          expect{ x.find_by_id }.to raise_error(Exception)
+          expect{ x.name }.to_not raise_error(Exception)
+        end
+        it "classes" do
+          x = class_double Project
+          allow(x).to receive(:first).and_return(nil)
+          expect{ x.asdsad }.to raise_error(Exception)
+          expect{ x.first }.to_not raise_error(Exception)
+        end
+        it "instances allowing the dynamic methods to be stubbed" do
+          x = object_double Project
+          allow(x).to receive(:find_by_id).and_return(nil)
+          expect{ x.find_by_id }.to_not raise_error(Exception)
+          expect{ x.find_by_iddd }.to raise_error(Exception)
+        end
+      end
+      context "spies" do
+        it "instances" do
+          x = instance_spy( Project)
+          expect{ x.find_by_id }.to raise_error(Exception)
+          expect{ x.name }.to_not raise_error(Exception)
+        end
+        it "classes" do
+          x = class_spy Project
+          expect{ x.asdsad }.to raise_error(Exception)
+          expect{ x.first }.to_not raise_error(Exception)
+        end
+        it "instances allowing the dynamic methods to be stubbed" do
+          x = object_spy Project
+          expect{ x.find_by_id }.to_not raise_error(Exception)
+          expect{ x.find_by_iddd }.to raise_error(Exception)
+        end
+      end
+    end
+  end
+
 end
